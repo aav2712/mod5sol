a536f5d9403c4d45c8e5a753e3bd26d50b0de92c
Arturo Arteaga Villarroel <Arturo Arteaga Villarroel>
2016-07-25 01:03:30 -0500

    test

--- js/script.js
+++ js/script.js
@@ -81,11 +81,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // *** start ***
 // On first load, show home view
 showLoading("#main-content");
+/*
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl, 
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  , // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
+*/
 // *** finish **
 
 
